16545ccad9cc36acac603d6e73ce9d8c4ebf3fe2
Yasemin <yacey@hotmail.de>
2024-10-02 16:19:38 +0200

    shlist created

---
+++ shoppinglist.py
@@ -0,0 +1,9 @@
+shoppinglist = []
+#create a shoppinglist shoppinglist.py
+#we ask the user "please insert the article wich you want into the shoppinglist"
+def add_item():
+   item = input ("please {insert} the article into the shoppinglist")
+   print(f"add your item") 
+   shoppinglist.append (item)
+
+add_item()
