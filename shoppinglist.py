753eaaf70ae153beba32beccdb26ca15fbaacca9
Yasemin <yacey@hotmail.de>
2024-10-03 11:24:50 +0200

    some fixes for shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -2,20 +2,20 @@ shoppinglist = []
 #create a shoppinglist shoppinglist.py
 #we ask the user "please insert the article wich you want into the shoppinglist"
 def add_item():
-   item = input ("please {insert} the article into the shoppinglist")
+   item = input ("please {insert} the article into the shoppinglist ")
    print(f"add your item") 
    shoppinglist.append (item)
 
 
 #Check the shoppinglist is empty
 def check_shoppinglist():
+   print("your shoppinglist:")
    if not shoppinglist:
       print("your shoppinglist is empty")
    else: 
-      print("your shoppinglist")
       for item in shoppinglist:
          print (item)
 
 
-add_item()
-check_shoppinglist()
+#add_item()
+
