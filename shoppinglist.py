911e5e546512bdb5012d517e6d7a9c5509fd2bb8
Yasemin <yacey@hotmail.de>
2024-10-11 09:38:27 +0200

    now

--- shoppinglist.py
+++ shoppinglist.py
@@ -20,8 +20,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS categories (
                name VARCHAR(32) NOT NULL)
                ''')
 
-# Activate foreign key
-cursor.execute("PRAGMA foreign_keys = ON;")
+
 
 def add_item():
     name = input("Which item are you looking for?: ")
@@ -36,41 +35,6 @@ def add_item():
         add_item()
     else:
         None
-
-def show_shoppinglist():
-    choice = input("Do you want all(1), a chosen(2) Items under a specific category(3) ? 1/2/3: ")
-    try:
-        if choice == '1':
-            cursor.execute("SELECT * FROM groceries")
-            list = cursor.fetchall()
-            print(list)
-        elif choice == '2':
-            second_choice = input("Are you looking for ID(1) or name(2) ? 1/2: ")
-            try:
-                if second_choice == '1':
-                    id = input("Please enter the ID: ")
-                    cursor.execute("SELECT * FROM groceries WHERE id = ?", (id,))
-                    item = cursor.fetchone()
-                    print(item)
-                elif second_choice == '2':
-                    name = input("Please enter the name: ")
-                    cursor.execute("SELECT * FROM groceries WHERE name = ?", (name,))
-                    name_item = cursor.fetchone()
-                    print(name_item)
-                elif choice == '3':
-                    category = input("Please enter the category: ")
-                    cursor.execute('''SELECT groceries.* FROM groceries
-                                        JOIN categories ON groceries.category_id = categories.id
-                                        WHERE categories.name = ?
-                                        ''', (category,))
-                    output = cursor.fetchall()
-                    for row in output:
-                        print(f"ID: {row[0]}, name: {row[1]}")
-
-            except ValueError:
-                print("Are you sure?")
-    except ValueError:
-        print("1 or 2...")
 
 def update_list():
     id = input("Please enter the ID: ")
@@ -91,34 +55,6 @@ def delete_item():
     cursor.execute("DELETE FROM groceries WHERE id = ?", (id,))
     print("deleted successfully!")
 
-def categories():
-    id = input("Please enter the ID: ")
-    category_name = input("Enter the category: ")
-    cursor.execute("INSERT INTO categories (name) VALUES (?)", (category_name,))
-    con.commit()
-    print(f "{name} added successfully!")
-
-def main_menu():
-    while True:
-        print("Welcome!\n1. Add\n2. Show\n3. Update\n4. Delete\n5. Category\n6. Finish")
-        choice = input("Enter a number: ")
-        try:
-            if choice == '1':
-                add_item()
-            elif choice == '2':
-                show_shoppinglist()
-            elif choice == '3':
-                update_list()
-            elif choice == '4':
-                delete_item()
-            elif choice == '5':
-                categories()
-            elif choice == '6':
-                quit()
-        except ValueError:
-            print("1, 2, 3, 4 or 5!")
-
-if __name__ == '__main__':
     main_menu()
 
 
